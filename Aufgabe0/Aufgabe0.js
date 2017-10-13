e1737a1a175feb97f9848aae34c36d2194fc54d4
Laura <Laura@LaurasPC.fritz.box>
2017-10-13 18:28:03 +0200

    mit namespace

--- Aufgabe0/Aufgabe0.js
+++ Aufgabe0/Aufgabe0.js
@@ -1,8 +1,11 @@
-function begruessung() {
-    var input = prompt("Bitte Namen eingeben:", "");
-    if (input != null) {
-        document.getElementById("hallo").innerHTML =
-            "Hallo " + input + "! Das ist eine persoenliche Begruessung...";
+var Aufgabe0;
+(function (Aufgabe0) {
+    function begruessung() {
+        var input = prompt("Bitte Namen eingeben:", "");
+        if (input != null) {
+            document.getElementById("hallo").innerHTML =
+                "Hallo " + input + "! Das ist eine persoenliche Begruessung...";
+        }
     }
-}
+})(Aufgabe0 || (Aufgabe0 = {}));
 //# sourceMappingURL=Aufgabe0.js.map

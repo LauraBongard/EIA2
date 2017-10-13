043506b08cd22b4c84f4a3a83926e084d72d2f2e
Laura <Laura@LaurasPC.fritz.box>
2017-10-13 21:54:12 +0200

    ohne namespace

--- Aufgabe0/Aufgabe0.js
+++ Aufgabe0/Aufgabe0.js
@@ -1,12 +1,11 @@
-var Aufgabe0;
-(function (Aufgabe0) {
-    function begruessung() {
-        var input;
-        input = prompt("Bitte Namen eingeben:", "");
-        if (input != null) {
-            document.getElementById("hallo").innerHTML =
-                "Hallo " + input + "! Das ist eine persoenliche Begruessung...";
-        }
+/*namespace Aufgabe0 {*/
+function begruessung() {
+    var input;
+    input = prompt("Bitte Namen eingeben:", "");
+    if (input != null) {
+        document.getElementById("hallo").innerHTML =
+            "Hallo " + input + "! Das ist eine persoenliche Begruessung...";
     }
-})(Aufgabe0 || (Aufgabe0 = {}));
+}
+/* }*/ 
 //# sourceMappingURL=Aufgabe0.js.map

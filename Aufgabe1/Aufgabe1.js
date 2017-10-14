e6aebf887ddfcc2c28769ebb27568c76262ccb18
Laura <Laura@LaurasPC.fritz.box>
2017-10-14 17:26:38 +0200

    pls now

--- Aufgabe1/Aufgabe1.js
+++ Aufgabe1/Aufgabe1.js
@@ -18,7 +18,7 @@ function init() {
     crc2.fillRect(0, 0, 800, 600);
     //Sonne
     crc2.beginPath();
-    crc2.arc(70, 70, 40, 0, 2 * Math.PI);
+    crc2.arc(50, 50, 40, 0, 2 * Math.PI);
     crc2.fillStyle = "#F4FA58";
     crc2.fill();
     //1.Wolke
@@ -28,12 +28,12 @@ function init() {
     crc2.fill();
     //2.Wolke
     crc2.beginPath();
-    crc2.arc(100, 90, 20, 0, 2 * Math.PI);
+    crc2.arc(100, 60, 20, 0, 2 * Math.PI);
     crc2.fillStyle = "#F2F2F2";
     crc2.fill();
     //3.Wolke
     crc2.beginPath();
-    crc2.arc(140, 120, 20, 0, 2 * Math.PI);
+    crc2.arc(140, 70, 20, 0, 2 * Math.PI);
     crc2.fillStyle = "#F2F2F2";
     crc2.fill();
     //1.Berg
@@ -77,11 +77,20 @@ function init() {
     crc2.lineTo(800, 350);
     crc2.stroke();
     crc2.closePath();
+    //Linien f�r Gondel
     crc2.beginPath();
     crc2.moveTo(650, 450);
     crc2.lineTo(700, 450);
-    crc2.closePath();
     crc2.stroke();
+    crc2.closePath();
+    crc2.beginPath();
+    crc2.moveTo(675, 450);
+    crc2.lineTo(675, 470);
+    crc2.stroke();
+    crc2.closePath();
+    //Gondel
+    crc2.fillStyle = "#151515";
+    crc2.fillRect(675, 470, 25, 10);
     /*crc2.fillRect(600, 150, 50, 30);
     crc2.fillStyle = "#0101DF";
     crc2.fill();

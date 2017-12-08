29ba68895e31c722817df5df6fded67a417b45f3
Laura <Laura@LaurasPC.eduroam.hs-furtwangen.de>
2017-12-08 10:08:17 +0100

    Aufg8 update

---
+++ Aufgabe8/Aufgabe8.js
@@ -0,0 +1,33 @@
+/*
+Aufgabe: 8, DynHTML: RandomBoxes
+Name: Laura Bongard
+Matrikel: 256028
+Datum: 08.12.17
+    
+Hiermit versichere ich, dass ich diesen Code selbst geschrieben habe. Er wurde nicht kopiert und auch nicht diktiert.
+*/
+var Aufgabe8;
+(function (Aufgabe8) {
+    window.addEventListener("load", input);
+    function input() {
+        let input = prompt("Gib eine Zahl zwischen 10 und 100 ein."); //input Variable ruft prompt Box auf
+        if (isNaN(parseInt(input)) || parseInt(input) < 10 || parseInt(input) > 100) {
+            alert("Deine Eingabe ist falsch.");
+        }
+        else {
+            for (let i = 0; i < parseInt(input); i++) {
+                initBox(Math.random() * window.innerWidth - 40, Math.random() * window.innerHeight - 40, Math.random() * 360);
+            }
+        }
+    }
+    function initBox(_x, _y, _color) {
+        let box = document.createElement("div"); //box Variable kreiert div Element
+        box.style.width = "40px";
+        box.style.height = "40px";
+        box.style.backgroundColor = "hsl(" + _color + ", 100%, 50%)";
+        box.style.left = _x + "px"; //_x & _y Parameter werden erst bei Funktionsaufruf in input Funktion festgelegt
+        box.style.top = _y + "px";
+        document.body.appendChild(box); //gezeichnete Box(Child) wird an das Body Element rangeh�ngt
+    }
+})(Aufgabe8 || (Aufgabe8 = {}));
+//# sourceMappingURL=Aufgabe8.js.map
